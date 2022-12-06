62e2dca7cc9c4b18cfe77aa6a0ff9bfcb6dff95a
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-06 23:45:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,10 +1,2 @@
-//
-// Created by ireti on 06/12/2022.
-//
-
-#ifndef ESERCITAZIONE_7_ESERCITAZIONE_6_H
-#define ESERCITAZIONE_7_ESERCITAZIONE_6_H
-
-#endif //ESERCITAZIONE_7_ESERCITAZIONE_6_H
 
 bool f (char &a);
