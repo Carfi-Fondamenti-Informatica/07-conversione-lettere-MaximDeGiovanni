6d50a57dbf7bf5c494c680b93503ce7913d11664
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-06 21:40:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+bool f(char &a) {
 
+    if ((a>=65 and a<=90) or (a >= 97 and a <=122)){
+
+        if (a>= 65 and a<=90) {
+            a+=32;
+        } else {
+            a-=32;
+
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
