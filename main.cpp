860a5f36076e214a153f34929ef9f730590d27af
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-06 21:44:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
+#include "lib.h"
+using namespace std;
+int main(){ char a;
+    cin>>a;
+    f(a);
+    if (f(a)==1) {
+        cout<<a<<endl;
+    } else {
+        cout<<"errore"<<endl;
+    }
   return 0;
 }
