58d69f88cd22bd46ca3db58dc41b3f7ca9bee466
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-07 15:23:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,20 +1,21 @@
-//
-// Created by ireti on 06/12/2022.
-//
 #include "esercitazione 6.h"
 
-bool f(char &a) {
+bool f(char &a)
+{
 
-    if ((a>=65 and a<=90) or (a >= 97 and a <=122)){
-
-        if (a>= 65 and a<=90) {
+    
+        if (a>= 65 and a<=90)
+        {
             a+=32;
-        } else {
-            a-=32;
-
+            return true;
         }
+       else if (a >= 97 and a <=122)
+       {
+            a-=32;
         return true;
-    } else {
+        } 
+        else
+        {
         return false;
-    }
+        }
 }
