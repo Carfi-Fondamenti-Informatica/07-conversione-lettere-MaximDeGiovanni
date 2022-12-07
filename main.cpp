4a6218faddaca0b4c400a05acf69fda020eddca0
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-07 15:21:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "esercitazione 6.h"
 using namespace std;
-int main() { char a;
+int main() {
+    char a=0;
     cin>>a;
     if (f(a)== true) {
         cout<<a<<endl;
